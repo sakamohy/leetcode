14762c73b37be34fd5587d2b92a0308cdb3a0655
sakahyu <sakahyu.eng@gmail.com>
2022-05-22 21:25:49 +0900

    this is first commit in leetcode

---
+++ problems/python3/1.two-sum.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        print("hello")
+
+
+# @lc code=end
