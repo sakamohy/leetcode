0a977337f2724a3db2eeae54ee508dd15915fb81
sakahyu <sakahyu.eng@gmail.com>
2022-05-23 02:04:02 +0900

    two-sum.py completegit add 1.two-sum.py !

--- problems/python3/1.two-sum.py
+++ problems/python3/1.two-sum.py
@@ -10,7 +10,13 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        print("hello")
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
+            else:
+                return [h[n], i]
 
 
 # @lc code=end
